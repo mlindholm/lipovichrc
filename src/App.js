448dba47b2baadceac8e0ae18a16db67ea967d74
Mathias Lindholm <mathias.lindholm@gmail.com>
2020-10-31 00:29:43 +0200

    Add register screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { ReactComponent as CloseIcon } from './images/close.svg'
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [drivers, setDrivers] = useState([{ id: count, name: '' }])
+
+  const updateDriver = id => event => {
+    const index = drivers.findIndex(driver => driver.id === id);
+    const newArray = [...drivers]
+    newArray[index] = { id, name: event.target.value }
+    setDrivers(newArray)
+  }
+
+  const addDriver = () => {
+    const newArray = [...drivers, { id: count + 1, name: '' }]
+    setDrivers(newArray)
+    setCount(count + 1)
+  }
+
+  const removeDriver = id => {
+    const newArray = [...drivers].filter(item => item.id !== id)
+    setDrivers(newArray)
+  }
+
+  const startRace = () => {
+    var newArray = drivers.filter(value => value.name !== '')
+    setDrivers(newArray)
+    console.log(newArray)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="RegisterDrivers">
+        <h3>Enter Drivers</h3>
+        {drivers.map(driver => (
+          <div key={driver.id} className="RegisterDrivers__Row">
+            <input
+              autoFocus
+              type="text"
+              value={driver.name}
+              onChange={updateDriver(driver.id)}
+              onKeyPress={event => event.key === 'Enter' && addDriver()}
+              />
+            <div className="RegisterDrivers__RemoveRow" onClick={() => removeDriver(driver.id)}>
+              <CloseIcon />
+            </div>
+          </div>
+          )
+          )}
+        <button onClick={() => addDriver()}>Add Driver</button>
+        <button onClick={() => startRace()}>Start Race</button>
+      </div>
     </div>
   );
 }
