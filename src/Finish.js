12d2e30ec25193f28434b913c4534f753e5e537d
Mathias Lindholm <mathias.lindholm@gmail.com>
2020-11-01 20:44:20 +0200

    Add count table to finish

--- src/Finish.js
+++ src/Finish.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { courseRules } from './courseRules'
+
+const pointsForId = (arr, id) => arr.find(v => v.id === Number(id)).points
 
 function Finish({drivers, restartFunc, undoEndFunc}) {
   return (
@@ -7,9 +10,23 @@ function Finish({drivers, restartFunc, undoEndFunc}) {
         <h2 className="Navigation__Title">Finished</h2>
       </div>
       <div className="Registration">
-        <div style={{background:'#eee',padding:16,marginBottom:16}}>
-          <code>{JSON.stringify(drivers)}</code>
-        </div>
+        <table>
+          <thead>
+            <th></th>
+            {drivers.map(driver => <th>{driver.name}</th>)}
+          </thead>
+          <tbody>
+            {courseRules.map(rule => (
+              <tr>
+                <td>{rule.name}, {rule.points > 0 && '+'}{rule.points}</td>
+                {drivers.map(driver => {
+                  const points = driver.points[rule.id] * rule.points || 0
+                  return <td>{points}</td>
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
         <button onClick={restartFunc}>Restart</button>&ensp;
         <button onClick={undoEndFunc}>Undo</button>
       </div>
