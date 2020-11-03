06f04287698326f2139bb7a923155145f38bf433
Mathias Lindholm <mathias.lindholm@gmail.com>
2020-11-03 22:17:34 +0200

    Add nicer styles for finish

--- src/Finish.js
+++ src/Finish.js
@@ -1,44 +1,64 @@
 import React from 'react'
+import './Finish.css'
 import { courseRules } from './courseRules'
 
 
 function Finish({drivers, restartFunc, undoEndFunc}) {
-  const medals = {
-    0: '🥇',
-    1: '🥈',
-    2: '🥉'
-  }
+  const medals = [
+    {icon: '🏆', color: '#ffec99'},
+    {icon: '🥈', color: '#e9ecef'},
+    {icon: '🥉', color: '#f0ddd1'},
+  ]
 
   return (
     <>
       <div className="Navigation">
         <h2 className="Navigation__Title">Finished</h2>
       </div>
-      <div className="Registration">
-        <table>
-          <thead>
-            <tr>
-              <th/>
-              {drivers.map((driver, i) => <th>{driver.name} {medals[i]}</th>)}
-            </tr>
-          </thead>
-          <tbody>
-            {courseRules.map(rule => (
+      <div className="Finish">
+        <div className="Finish__Podium">
+          {drivers.slice(0,3).map((driver, i) => (
+            <div className="Podium__Entry" style={{backgroundColor: medals[i].color}}>
+              <div class="Podium__Medal">
+                {medals[i].icon}
+              </div>
+              <div class="Podium__Name">
+                {driver.name}
+              </div>
+              <div class="Podium__Points">
+                {driver.total} points
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="Finish__TableContainer">
+          <table className="Finish__Table">
+            <thead>
               <tr>
-                <td>{rule.name} <small style={{color:'gray'}}>{rule.points > 0 && '+'}{rule.points}</small></td>
-                {drivers.map(driver => <td>{driver.points[rule.id] * rule.points || 0}</td>)}
+                <th>Brakedown</th>
+                {drivers.map(driver => <th>{driver.name}</th>)}
               </tr>
-            ))}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td>Total</td>
-              {drivers.map(driver => <td>{driver.total}</td>)}
-            </tr>
-          </tfoot>
-        </table>
-        <button onClick={restartFunc}>New Competition</button>&ensp;
-        <button className="Footer__SecondaryButton" onClick={undoEndFunc}>Return to Competition</button>
+            </thead>
+            <tbody>
+              {courseRules.map(rule => (
+                <tr>
+                  <td>{rule.name} <small style={{color:'gray'}}>{rule.points > 0 && '+'}{rule.points}</small></td>
+                  {drivers.map(driver => <td>{driver.points[rule.id] * rule.points || 0}</td>)}
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td>Total</td>
+                {drivers.map(driver => <td><strong>{driver.total}</strong></td>)}
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <div className="Finish__Footer">
+          <button className="Finish__Button" onClick={restartFunc}>New Competition</button>
+          <button className="Finish__SecondaryButton" onClick={undoEndFunc}>Return to Competition</button>
+        </div>
       </div>
     </>
   )
