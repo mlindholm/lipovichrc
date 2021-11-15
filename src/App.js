f87f9bea69a1f4d6bbda0f9c68e952bf4770606d
Mathias Lindholm <mathias.lindholm@gmail.com>
2021-11-15 15:29:29 +0200

    Add wrapping div

--- src/App.js
+++ src/App.js
@@ -9,34 +9,35 @@ import Finish from './views/Finish'
 import InstallPWA from './pwa/InstallPWA'
 import './App.css'
 
-
 function App() {
   return (
-    <SpeechProvider appId="a16e8861-7891-4af4-99a8-c7dd2b674eaa" language="en-US">
-      <BrowserRouter>
-        <ScrollToTop />
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/register" />
-          </Route>
-          <Route path="/register">
-            <Registration />
-            <InstallPWA />
-          </Route>
-          <Route path="/compete">
-            <BigTranscript highlightColor="#f4882a" backgroundColor="#17191c" formatText={false} />
-            <Competition />
-            <PushToTalkButton placement="bottom" size="72px" gradientStops={['#f6b035', '#f4882a']} fontSize="1rem" backgroundColor="#17191c" />
-          </Route>
-          <Route path="/finish">
-            <Finish />
-          </Route>
-          <Route path="*">
-            <Redirect to="/compete" />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </SpeechProvider>
+    <div class="App">
+      <SpeechProvider appId="a16e8861-7891-4af4-99a8-c7dd2b674eaa" language="en-US">
+        <BrowserRouter>
+          <ScrollToTop />
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/register" />
+            </Route>
+            <Route path="/register">
+              <Registration />
+              <InstallPWA />
+            </Route>
+            <Route path="/compete">
+              <BigTranscript highlightColor="#f4882a" backgroundColor="#17191c" formatText={false} />
+              <Competition />
+              <PushToTalkButton placement="bottom" size="72px" gradientStops={['#f6b035', '#f4882a']} fontSize="1rem" backgroundColor="#17191c" />
+            </Route>
+            <Route path="/finish">
+              <Finish />
+            </Route>
+            <Route path="*">
+              <Redirect to="/compete" />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </SpeechProvider>
+    </div>
   )
 }
 
